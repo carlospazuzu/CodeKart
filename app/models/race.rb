2ddea6a6a37121dcbb4d1b3977170d114d29b883
carlospazuzu <carlosh555@hotmail.com>
2023-02-27 13:12:18 -0300

    refactor: improving specs legibility, removing unused methods and adding missing verifications

--- app/models/race.rb
+++ app/models/race.rb
@@ -7,8 +7,4 @@ class Race < ApplicationRecord
   accepts_nested_attributes_for :placements
 
   belongs_to :tournament
-
-  def add_racer_placement(racer_id, position)
-    Placement.create({position: position, racer_id: racer_id, race_id: self.id})
-  end
 end
--- spec/models/racer_spec.rb
+++ spec/models/racer_spec.rb
@@ -5,21 +5,30 @@ RSpec.describe Racer, type: :model do
     it { is_expected.to validate_presence_of(:name)    }
     it { is_expected.to validate_presence_of(:born_at) }
 
-    context 'when trying to register an underaged racer' do
+    context 'when racer is underaged (18 years old)' do
       let(:underage_racer) { build(:racer, :underaged) }
 
-      it 'marks the object as invalid' do
+      it 'returns an invalid racer' do
         expect(underage_racer.invalid?).to be true
       end
 
-      it 'generates an error object in the errors array' do
+      it 'returns an error' do
         underage_racer.valid?
         expect(underage_racer.errors[:born_at].first).to eq('Need to be at least 18 years old!')
+      end
+    end
+
+    context 'when racer have a legal age' do
+      let(:legal_racer) { build(:racer) }
+
+      it 'returns a valid racer' do
+        expect(legal_racer.valid?).to be true
       end
     end
   end
 
   describe 'Associations' do
-    it { is_expected.to have_many(:races) }
+    it { is_expected.to have_many(:races)      }
+    it { is_expected.to have_many(:placements) }
   end
 end
