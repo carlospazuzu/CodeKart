fa94b1d96bfe463cdc7dd027d80b80184bd00bfc
carlospazuzu <carlosh555@hotmail.com>
2023-02-27 10:16:34 -0300

    refactor: removing unrelated specs and refactoring racer model

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,6 +1,8 @@
 class Racer < ApplicationRecord
-  validates :name, :born_at, presence: true
-  validates_with LegalAgeValidator
+  validates :name, presence: true
+  validates :born_at, presence: true,
+                      comparison: { less_than_or_equal_to: 18.years.ago,
+                                    message: 'Need to be at least 18 years old!' }
 
   has_many :placements, dependent: :destroy
   has_many :races, through: :placements, dependent: :destroy
--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -4,22 +4,10 @@ RSpec.describe Race, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:date)  }
     it { is_expected.to validate_presence_of(:place) }
-
-    context 'when attributing a racer two different placements in the same race' do
-      it 'rolls back the conflicting operation and notifies that a racer can occupy only one position per race' do
-
-        racer = create(:racer)
-        race = create(:race_with_tournament)
-
-        race.add_racer_placement(racer.id, 1)
-        race = race.add_racer_placement(racer.id, 2)
-
-        expect(race.errors[:racer_id].first).to eq('A racer can occupy only one position per race!')
-      end
-    end
   end
 
   describe 'Associations' do
+    it { is_expected.to have_many(:placements) }
     it { is_expected.to have_many(:racers)     }
     it { is_expected.to belong_to(:tournament) }
   end
